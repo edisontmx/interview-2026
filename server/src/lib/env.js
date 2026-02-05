78576715950171f89b1a79699703833802a1f60b
chuck-dev <edison.mc@gmail.com>
2026-02-04 19:50:11 -0600

    configuracion de despliegue

--- server/src/lib/env.js
+++ server/src/lib/env.js
@@ -1,3 +1,6 @@
 export const ENV = {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL,
+    NODE_ENV: process.env.NODE_ENV || 'production'
+
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,29 @@
 import express from 'express'
+import path from 'path'
+
 import { ENV } from './lib/env.js'
 
 const app = express()
 
+const __dirname = path.resolve()
+
 console.log(process.env.PORT)
 
-app.get("/", (req, res) => {
-    res.status(200).json({msg:"hello world from the api new"})
+app.get("/hello", (req, res) => {
+    res.status(200).json({ msg: "hello world from the api new" })
 })
 
-app.listen(ENV.PORT, () => console.log("server is tunning on port 3000"))
+app.get("/books", (req, res) => {
+    res.status(200).json({ msg: "books endpoint" })
+})
+
+//coniguracion para despliegue
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../client/dist")))
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/dist"))
+    })
+}
+
+app.listen(ENV.PORT, () => console.log("server is running on port 3000"))
